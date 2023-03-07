8b808dbff2d05712550eb07fdea235a2f2ed45ec
mahadtoukaleh <94094060+mahadtoukaleh@users.noreply.github.com>
2023-03-06 19:17:16 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
 function checkStock() {
+  const keywords = [
+          "finance", "banking", "beer", "pork", "gambling", "betting", "lottery", "interest", "usury", "ribaa", "pornography", "prostitution", "drugs", "tobacco", "weapons", "violence", "war", "terrorism", "fraud", "theft", "embezzlement", "bribery", "corruption", "exploitation", "oppression", "injustice", "discrimination", "slavery", "human trafficking", "animal cruelty", "environmental destruction", "usurpation of rights"
+     ];
+    const haramSectors = [
+          "Alcohol and tobacco production and distribution",
+          "Gambling and casino operations",
+          "Conventional financial institutions and banking",
+          "Adult entertainment industry",
+          "Weapons and defense contractors",
+          "Pork and other non-halal meat production and         distribution",
+          "Interest-based loan providers and credit card companies"
+    ];
     const symbol = document.getElementById("symbol").value;
     const apiKey = "Y03F3NU4L6AOFHIUZ";
     const apiUrl = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=" + symbol +"&apikey=Y03F3NU4L6AOFHIUZ";
@@ -10,15 +22,20 @@ function checkStock() {
         const description = data.Description.toLowerCase();
         let result;
   
-        if (sector.includes("finance") || sector.includes("banking")) {
-          result = "haram";
-        } else if (description.includes("gambling") || description.includes("alcohol") || description.includes("beer") || description.includes("tobacco") || description.includes("lottery")) {
-          result = "haram";
-        } else {
-          result = "halal";
+        for(let i = 0; i < haramSectors.length; i++) {
+          if(sector.includes(haramSectors[i])) {
+            result = "Haram because it is in the sector of " + haramSectors[i];
+          }
         }
   
-        setResult(symbol + ` is: ${result}`);
+        for(let i = 0; i < keywords.length; i++) {
+          if(description.includes(keywords[i])) {
+            result = "Haram because it contains " + keywords[i];
+            break;
+          }
+        }
+  
+        setResult(symbol + " is: " + (result || "Halal"));
       })
       .catch(error => {
         console.error("Error:", error);
@@ -30,4 +47,5 @@ function checkStock() {
     resultElement.textContent = message;
     resultElement.parentNode.style.display = "block";
   }
+
   
